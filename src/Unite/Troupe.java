728a5a5b85c6a94028c5bb227abd1ec410b822d6
Jeremy <jeremy.hulot@gmail.com>
2014-11-25 14:46:09 +0100

    On surcharge les méthodes d'actions pour Troupe

--- src/Unite/Troupe.java
+++ src/Unite/Troupe.java
@@ -1,9 +1,15 @@
 package Unite;
 import java.util.ArrayList;
+import java.util.Iterator;
+
+import Plateau.Position;
 
 public class Troupe extends Unite {
 
 	private ArrayList<Unite> listedUnite = new ArrayList<Unite>();
+	
+	public Troupe() {
+	}
 	
     public void addChild(Unite unite) {
     	listedUnite.add(unite);
@@ -16,4 +22,36 @@ public class Troupe extends Unite {
     public Unite getChild(int index) {
     	return listedUnite.get(index);
     }
+    
+    public void seDeplacer(Position _position) {
+    	System.out.println("La troupe se déplace.");
+    	for(Iterator<Unite> iterator = listedUnite.iterator(); iterator.hasNext();) {
+    		Unite unite = iterator.next();
+    		
+    		if(unite != null) {
+    			System.out.println(unite.toString() + ": ");
+    			unite.seDeplacer(_position);
+    		}
+    	}
+	}
+	
+	public void combattre() {
+		for(Iterator<Unite> iterator = listedUnite.iterator(); iterator.hasNext();) {
+    		Unite unite = iterator.next();
+    		
+    		if(unite != null) {
+    			unite.combattre();
+    		}
+    	}
+	}
+	
+	public void defendre() {
+		for(Iterator<Unite> iterator = listedUnite.iterator(); iterator.hasNext();) {
+    		Unite unite = iterator.next();
+    		
+    		if(unite != null) {
+    			unite.defendre();
+    		}
+    	}
+	}
 }
